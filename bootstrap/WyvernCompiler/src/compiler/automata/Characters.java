b6f3766dbcd36a8c9270c2133cf0a8f7fdb40918
Michael Adelson <mike.adelson314@gmail.com>
2011-12-29 18:54:16 -0500

    Characters implementation which only handles ranges in the gaps between characters from concrete sets.
    I want to refactor this code to simplify it, but this commit will save the history of this implementation so that I can get it back if needed.

--- bootstrap/WyvernCompiler/src/compiler/automata/Characters.java
+++ bootstrap/WyvernCompiler/src/compiler/automata/Characters.java
@@ -4,14 +4,17 @@
 package compiler.automata;
 
 import java.util.AbstractCollection;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import compiler.Utils;
@@ -24,126 +27,201 @@ public class Characters {
 	private static final SetOperations<Character> simpleSetOperations = new SimpleSetOperations<Character>();
 
 	private static final SetOperations<Character> setOperations = new SetOperations<Character>() {
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * compiler.automata.SetOperations#partitionedUnion(java.util.Collection
-		 * )
-		 */
+
 		@Override
 		public Set<Collection<Character>> partitionedUnion(
 				Collection<Collection<Character>> sets) {
-			List<Collection<Character>> result = new ArrayList<Collection<Character>>(
-					sets);
-			boolean changed;
-			do {
-				changed = false;
-
-				OUTER: for (int i = 0; i < result.size(); i++) {
-					for (int j = i + 1; j < result.size(); j++) {
-						Set<Collection<Character>> pairResult = this
-								.partitionedUnion(result.get(i), result.get(j));
-						if (pairResult != null) {
-							result.remove(j); // remove at j first since j > i
-							result.remove(i);
-							result.addAll(pairResult);
-							changed = true;
-							break OUTER;
-						}
+			/*
+			 * Organize the given sets by their members. Ranges are collected
+			 * separately for efficiency
+			 */
+			Set<Range> ranges = new HashSet<Range>();
+			SortedMap<Character, Set<Collection<Character>>> collectionsByCharacter = new TreeMap<Character, Set<Collection<Character>>>();
+			for (Collection<Character> set : sets) {
+				if (set instanceof Range) {
+					ranges.add((Range) set);
+				} else {
+					for (Character ch : set) {
+						Utils.put(collectionsByCharacter, HashSet.class, ch,
+								set);
 					}
 				}
-			} while (changed);
+			}
 
-			return new HashSet<Collection<Character>>(result);
+			// partition the ranges
+
+			Set<Collection<Character>> result = new LinkedHashSet<Collection<Character>>();
+
+			// find any ranges before the first character
+			Character lowerBound = null;
+			Character upperBound = collectionsByCharacter.isEmpty() ? null
+					: collectionsByCharacter.firstKey();
+			Set<Range> activeRanges = this.getActiveRanges(ranges, lowerBound,
+					upperBound);
+			result.addAll(this.partitionedRangeUnion(activeRanges, lowerBound,
+					upperBound));
+
+			// scan through the collected characters and ranges, adding
+			// collections as necessary
+			Set<Collection<Character>> currentSets = new HashSet<Collection<Character>>();
+			Set<Character> currentChars = new HashSet<Character>();
+			Character prev = null;
+			for (Character ch : collectionsByCharacter.keySet()) {
+				// find ranges between the last character and the current one
+				if (prev != null) {
+					activeRanges = this.getActiveRanges(ranges, prev, ch);
+					result.addAll(this.partitionedRangeUnion(activeRanges,
+							prev, ch));
+				}
+
+				// if the current character is associated with all the same sets
+				// as the previous one, just add it
+				if (collectionsByCharacter.get(ch).equals(currentSets)) {
+					currentChars.add(ch);
+				}
+
+				// otherwise, create a set for all previous characters (if any)
+				// and save the current character and collection set
+				else {
+					if (!currentChars.isEmpty()) {
+						result.add(currentChars);
+						currentChars = new HashSet<Character>();
+						currentSets.clear();
+					}
+					currentChars.add(ch);
+					currentSets.addAll(collectionsByCharacter.get(ch));
+				}
+
+				prev = ch;
+			}
+
+			// finish creating any remaining sets
+			if (!currentChars.isEmpty()) {
+				result.add(currentChars);
+			}
+
+			/*
+			 * find ranges between the last character and the end of the
+			 * alphabet Note that this won't run for an empty
+			 * collectionsByCharacter set, which is what we want since in that
+			 * case the
+			 */
+			if (prev != null) {
+				activeRanges = this.getActiveRanges(ranges, prev, null);
+				result.addAll(this.partitionedRangeUnion(activeRanges, prev,
+						null));
+			}
+		}
+
+		private Set<Range> getActiveRanges(Set<Range> ranges,
+				Character lowerBound, Character upperBound) {
+			if (ranges.isEmpty()) {
+				return Collections.emptySet();
+			}
+
+			Set<Range> activeRanges = new HashSet<Range>();
+			for (Range range : ranges) {
+				if ((lowerBound == null || range.min() > lowerBound)
+						&& (upperBound == null || range.max() < upperBound)) {
+					activeRanges.add(range);
+				}
+			}
+
+			return activeRanges;
 		}
 
 		/**
-		 * Performs the partitioned union operation on just two sets, returning
-		 * null the result would be { set1, set2 }.
+		 * As normal range partitioning, but operates only on a set of Ranges
 		 */
-		private Set<Collection<Character>> partitionedUnion(
-				Collection<Character> set1, Collection<Character> set2) {
-			// optimize for ranges
-			if (set1 instanceof Range) {
-				Range range1 = (Range) set1;
-
-				// range overlap
-				if (set2 instanceof Range) {
-					Range range2 = (Range) set2;
-					if (range1.overlaps(range2)) {
-						char[] boundaries = new char[] { range1.min(),
-								range1.max(), range2.min(), range2.max() };
-						Arrays.sort(boundaries);
-						Set<Collection<Character>> result = new HashSet<Collection<Character>>();
-						
-						// wrong
-						for (int i = 0; i < boundaries.length - 1; i++) {
-							if (boundaries[i] != boundaries[i + 1]) {
-								result.add(range((char) (boundaries[i] + 1),
-										boundaries[i + 1]));
-							}
-						}
-
-						return result;
-					}
-					return null; // no change
-				}
-
-				// normal case
-				Collection<Character> beforeChars = new ArrayList<Character>(), afterChars = new ArrayList<Character>(), overlapChars = new ArrayList<Character>();
-				for (Character ch : set2) {
-					if (ch < range1.min()) {
-						beforeChars.add(ch);
-					} else if (ch > range1.max()) {
-						afterChars.add(ch);
-					} else {
-						overlapChars.add(ch);
-					}
-				}
-
-				if (overlapChars.isEmpty()) {
-					return null; // no change
-				}
-
-				Set<Collection<Character>> result = new HashSet<Collection<Character>>();
-				if (!beforeChars.isEmpty()) {
-					result.add(beforeChars);
-				}
-				if (!afterChars.isEmpty()) {
-					result.add(afterChars);
-				}
-
-				overlapChars.add(range1.min());
-				overlapChars.add(range1.max());
-				// sort and dedup
-				overlapChars = new TreeSet<Character>(overlapChars);
-
-				// wrong
-				Character prev = null;
-				for (Character ch : overlapChars) {
-					if (prev != null && ch > prev) {
-						result.add(new Range(prev, ch));
-						prev = (char) (ch + 1);
-					} else {
-						prev = ch;
-					}
-				}
-				Utils.check(!result.isEmpty()); // sanity check
-				return result;
+		private Set<Collection<Character>> partitionedRangeUnion(
+				Collection<Range> ranges, Character lowerBound,
+				Character upperBound) {
+			if (ranges.isEmpty()) {
+				return Collections.emptySet();
 			}
 
-			// be indifferent to argument order
-			if (set2 instanceof Range) {
-				return this.partitionedUnion(set2, set1);
+			// associate each point with the number of ranges which start and
+			// end on it
+			SortedMap<Character, Integer> minValues = new TreeMap<Character, Integer>(), maxValues = new TreeMap<Character, Integer>();
+			SortedSet<Character> allValues = new TreeSet<Character>();
+			for (Range range : ranges) {
+				Range trimmed = this.trim(range, lowerBound, upperBound);
+				minValues.put(trimmed.min(),
+						Utils.getOrDefault(minValues, trimmed.min(), 0) + 1);
+				maxValues.put(trimmed.max(),
+						Utils.getOrDefault(maxValues, trimmed.max(), 0) + 1);
+				allValues.add(trimmed.min());
+				allValues.add(trimmed.max());
 			}
 
-			// no ranges, so just use the simple set operations logic
-			List<Collection<Character>> sets = new ArrayList<Collection<Character>>(
-					2);
-			sets.add(set1);
-			sets.add(set2);
-			return simpleSetOperations.partitionedUnion(sets);
+			/*
+			 * Scan through the points, creating a range when some range covers
+			 * that region.
+			 */
+			Set<Collection<Character>> result = new LinkedHashSet<Collection<Character>>();
+			// the last char which is not yet represented by a set in result
+			char lastUnrepresentedChar = allValues.first();
+			// the number of ranges which are active at lastUnrepresentedChar
+			int activeRangeCount = 0;
+			for (Character ch : allValues) {
+				int startingRangeCount = Utils.getOrDefault(minValues, ch, 0);
+				int endingRangeCount = Utils.getOrDefault(maxValues, ch, 0);
+
+				/*
+				 * If ranges start at ch, we need a gap range through ch - 1 if
+				 * there are active ranges and we need a singleton set for ch if
+				 * ranges also end at ch.
+				 */
+				if (startingRangeCount > 0) {
+					if (activeRangeCount > 0) {
+						// sanity check
+						Utils.check(lastUnrepresentedChar <= ch - 1);
+
+						result.add(lastUnrepresentedChar < ch - 1 ? range(
+								lastUnrepresentedChar, (char) (ch - 1))
+								: Collections.singleton((char) (ch - 1)));
+					}
+
+					if (endingRangeCount > 0) {
+						Utils.check(activeRangeCount > 0); // sanity check
+
+						result.add(Collections.singleton(ch));
+						lastUnrepresentedChar = (char) (ch + 1); // ch already
+																	// represented
+					} else {
+						lastUnrepresentedChar = ch;
+					}
+				}
+				/*
+				 * If ranges just end at ch, we need a gap range through ch.
+				 */
+				else {
+					Utils.check(endingRangeCount > 0); // sanity check
+					Utils.check(activeRangeCount > 0); // sanity check
+
+					result.add(lastUnrepresentedChar == ch ? Collections
+							.singleton(ch) : range(lastUnrepresentedChar, ch));
+					lastUnrepresentedChar = (char) (ch + 1); // ch already
+																// represented
+				}
+
+				activeRangeCount += (startingRangeCount - endingRangeCount);
+			}
+
+			return result;
+		}
+
+		private Range trim(Range range, Character lowerBound,
+				Character upperBound) {
+			char min = lowerBound != null ? (char) Math.max(range.min(),
+					lowerBound) : range.min();
+			char max = upperBound != null ? (char) Math.min(range.max(),
+					upperBound) : range.max();
+
+			if (min == range.min() && max == range.max()) {
+				return range;
+			}
+			return range(min, max);
 		}
 	};
 
