002237859123bd5dc36ada8f8b66fdd676124402
Michael Adelson <mike.adelson314@gmail.com>
2013-05-02 06:34:08 -0400

    Changed LR0Generator's closure to use the faster version now used in LR1Generator's closure.

--- bootstrap/WyvernCompiler/src/compiler/parse/LR0Generator.java
+++ bootstrap/WyvernCompiler/src/compiler/parse/LR0Generator.java
@@ -21,18 +21,38 @@ public class LR0Generator extends LRGenerator {
 	 */
 	@Override
 	protected Set<Item> closure(Grammar grammar, Set<Item> items) {
-		boolean changed;
-		do {
-			changed = false;
-
-			for (Item item : new ArrayList<Item>(items))
-				if (item.hasNextSymbolType()
-						&& !item.nextSymbolType().isTerminal()) {
-					for (Production production : grammar.productions(item
-							.nextSymbolType()))
-						changed |= items.add(new Item(production, null, 0));
-				}
-		} while (changed);
+		/*
+		 * The official algorithm is:
+		 * Closure(I) = 
+		 * repeat
+		 * 	for any item (A -> _.XB) in I
+		 * 		for any production X -> y
+		 * 			I = I U {(X -> .y)}
+		 * until I does not change
+		 * return I
+		 * 
+		 * However, notice that looping over any item twice will always yield the same set
+		 * of productions. Thus, rather than repeatedly looping over the entirety of I we instead
+		 * just iterate from i = 0 -> I.size(), where I.size() grows as we add more items. Whenever
+		 * I stops growing, i will catch up and the algorithm will terminate.
+		 */
+		
+		// for any item A -> _.XB in items
+		List<Item> itemList = new ArrayList<Item>(items);
+		for (int i = 0; i < itemList.size(); ++i) {
+			Item item = itemList.get(i);
+			if (item.hasNextSymbolType() && !item.nextSymbolType().isTerminal()) {
+				// for any production X -> .something
+				for (Production production : grammar.productions(item.nextSymbolType())) {
+					// items <- items U { X -> .something }
+					Item newItem = new Item(production, null, 0);
+					if (items.add(newItem)) {
+						// if we found a new item, queue it for iteration
+						itemList.add(newItem);
+					}
+ 				}
+			}
+		}
 
 		return items;
 	}
