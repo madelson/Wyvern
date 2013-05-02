c75c668998c788eb233ea9b2e8ffd4cf4146f49b
Michael Adelson <mike.adelson314@gmail.com>
2013-05-01 22:03:41 -0400

    Optimized the LR1Generator's implementation of closure() by avoiding reiterating over Items more than once (since processing an item always leads to the same set of produced items).
    This was done after determining that closure() was a very time-consuming part of the algorithm's inner loop.

--- bootstrap/WyvernCompiler/src/compiler/parse/LR1Generator.java
+++ bootstrap/WyvernCompiler/src/compiler/parse/LR1Generator.java
@@ -21,27 +21,42 @@ public class LR1Generator extends LR0Generator {
 	 */
 	@Override
 	protected Set<Item> closure(Grammar grammar, Set<Item> items) {
-		boolean changed;
-		// repeat
-		do {
-			changed = false;
-
-			// for any item A -> _.X?, z in items
-			for (Item item : new ArrayList<Item>(items))
-				if (item.hasNextSymbolType()
-						&& !item.nextSymbolType().isTerminal()) {
-					// for any production X -> .something
-					for (Production production : grammar
-							.productions(item.nextSymbolType())) {
-						// for any w in FIRST(?z)
-						for (SymbolType tokenType : grammar.nff().first(
-								item.remaining()))
-							// items <- items U { X -> .something, w }
-							changed |= items.add(new Item(production,
-									tokenType, 0));
-					}
-				}
-		} while (changed);
+		/*
+		 * The official algorithm is:
+		 * Closure(I) = 
+		 * repeat
+		 * 	for any item (A -> _.XB, z) in I
+		 * 		for any production X -> y
+		 * 			for any w in FIRST(Bz)
+		 * 				I = I U {(X -> .y, w)}
+		 * until I does not change
+		 * return I
+		 * 
+		 * However, notice that looping over any item twice will always yield the same set
+		 * of productions. Thus, rather than repeatedly looping over the entirety of I we instead
+		 * just iterate from i = 0 -> I.size(), where I.size() grows as we add more items. Whenever
+		 * I stops growing, i will catch up and the algorithm will terminate.
+		 */
+		
+		// for any item A -> _.XB, z in items
+		List<Item> itemList = new ArrayList<Item>(items);
+		for (int i = 0; i < itemList.size(); ++i) {
+			Item item = itemList.get(i);
+			if (item.hasNextSymbolType() && !item.nextSymbolType().isTerminal()) {
+				// for any production X -> .something
+				for (Production production : grammar.productions(item.nextSymbolType())) {
+					// for any w in FIRST(Bz)
+					for (SymbolType tokenType : grammar.nff().first(item.remaining())) {
+						// items <- items U { X -> .something, w }
+						Item newItem = new Item(production, tokenType, 0);
+						if (items.add(newItem)) {
+							// if we found a new item, queue it for iteration
+							itemList.add(newItem);
+						}
+ 					}
+ 				}
+			}
+		}
 
 		return items;
 	}
